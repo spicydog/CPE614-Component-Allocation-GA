d06e298457de8328a94763051b1bc5de2fe3923a
Spicydog Proxy <spicydog@gmail.com>
2014-10-22 22:56:12 +0700

    First commit

---
+++ src/org/spicydog/Algorithm.java
@@ -0,0 +1,89 @@
+package org.spicydog;
+
+/**
+ * Created by spicydog on 10/21/14.
+ * Based on http://www.theprojectspot.com/tutorial-post/creating-a-genetic-algorithm-for-beginners/3
+ */
+public class Algorithm {    /* GA parameters */
+    private static double uniformRate = Config.defaultUniformRate;
+    private static double mutationRate = Config.defaultMutationRate;
+    private static int tournamentSize = Config.defaultTournamentSize;
+    private static int elitismOffset = Config.defaultElitismOffset;
+
+
+    /* Public methods */
+
+    // Evolve a population
+    public static Population evolvePopulation(Population pop) {
+        Population newPopulation = new Population(pop.size(), false);
+
+        // Keep our best individual
+        for (int i = 0; i < elitismOffset; i++) {
+            newPopulation.saveIndividual(i, pop.getFittest());
+        }
+
+        // Crossover population
+//        int elitismOffset;
+//        if (isElitism) {
+//            elitismOffset = 1;
+//        } else {
+//            elitismOffset = 0;
+//        }
+        // Loop over the population size and create new individuals with
+        // crossover
+        for (int i = elitismOffset; i < pop.size(); i++) {
+            Individual indiv1 = tournamentSelection(pop);
+            Individual indiv2 = tournamentSelection(pop);
+            Individual newIndiv = crossover(indiv1, indiv2);
+            newPopulation.saveIndividual(i, newIndiv);
+        }
+
+        // Mutate population
+        for (int i = elitismOffset; i < newPopulation.size(); i++) {
+            mutate(newPopulation.getIndividual(i));
+        }
+
+        return newPopulation;
+    }
+
+    // Crossover individuals
+    private static Individual crossover(Individual indiv1, Individual indiv2) {
+        Individual newSol = new Individual();
+        // Loop through genes
+        for (int i = 0; i < indiv1.size(); i++) {
+            // Crossover
+            if (Math.random() <= uniformRate) {
+                newSol.setGene(i, indiv1.getGene(i));
+            } else {
+                newSol.setGene(i, indiv2.getGene(i));
+            }
+        }
+        return newSol;
+    }
+
+    // Mutate an individual
+    private static void mutate(Individual indiv) {
+        // Loop through genes
+        for (int i = 0; i < indiv.size(); i++) {
+            if (Math.random() <= mutationRate) {
+                // Create random gene
+                byte gene = (byte) Math.round(Math.random());
+                indiv.setGene(i, gene);
+            }
+        }
+    }
+
+    // Select individuals for crossover
+    private static Individual tournamentSelection(Population pop) {
+        // Create a tournament population
+        Population tournament = new Population(tournamentSize, false);
+        // For each place in the tournament get a random individual
+        for (int i = 0; i < tournamentSize; i++) {
+            int randomId = (int) (Math.random() * pop.size());
+            tournament.saveIndividual(i, pop.getIndividual(randomId));
+        }
+        // Get the fittest
+        Individual fittest = tournament.getFittest();
+        return fittest;
+    }
+}
---
+++ src/org/spicydog/Config.java
@@ -0,0 +1,18 @@
+package org.spicydog;
+
+/**
+ * Created by spicydog on 10/22/14.
+ */
+public class Config {
+
+    protected static String solution = "1111000000000000000000000000000000000000000000000000000000001111";
+    protected static int defaultGeneLength = 64;
+    protected static int defaultPopulationSize = 50;
+
+    protected static double defaultUniformRate = 0.5;
+    protected static double defaultMutationRate = 0.015;
+    protected static int defaultTournamentSize = 5;
+    protected static int defaultElitismOffset = 1;
+
+
+}
