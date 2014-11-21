20f93ab9038b6c603f64eaf6bcaf5b75eeccd02a
Spicydog Proxy <spicydog@gmail.com>
2014-11-21 20:51:13 +0700

    Ready to make a report

--- src/org/spicydog/Main.java
+++ src/org/spicydog/Main.java
@@ -7,11 +7,17 @@ public class Main {
 
     public static void main(String[] args) {
 
+        String strAlpha = "";
+        String strBeta = "";
+        String strGamma = "";
+        String strReliability = "";
+        String strCost = "";
+        String strWeight = "";
 
-        for (int x = 1; x <= 100; x++) {
-            for (int y = 0; y <= 100; y++) {
-                for (int z = 0; z <= 100; z++) {
-                    if(x+y+z==100) {
+        for (int x = 1; x <= 10; x++) {
+            for (int y = 0; y <= 10; y++) {
+                for (int z = 0; z <= 10; z++) {
+                    if(x+y+z==10) {
 
 
                         int n = Config.nRun;
@@ -19,9 +25,13 @@ public class Main {
                         double[] resultTime = new double[n];
                         Individual[] resultIndividual = new Individual[n];
 
-                        Config.alpha   = (double) x/100;
-                        Config.beta    = (double) y/100;
-                        Config.gamma = (double) z/100;
+                        Config.alpha   = (double) x/10;
+                        Config.beta    = (double) y/10;
+                        Config.gamma = (double) z/10;
+
+                        double sumReliality = 0;
+                        double sumCost = 0;
+                        double sumWeight = 0;
 
 
                         for (int iRun = 0; iRun < n; iRun++) {
@@ -31,7 +41,7 @@ public class Main {
                             double fitness = Double.MIN_VALUE;
                             double lastFitness = fitness;
                             boolean isSolutionFound = false;
-                            int solutionFoundAtGeneration = 0;
+                            int solutionFoundAtGeneration = -1;
 
                             long startTime = System.nanoTime();
                             long endTime = startTime;
@@ -72,6 +82,10 @@ public class Main {
                                 resultGeneration[iRun] = solutionFoundAtGeneration;
                                 resultTime[iRun] = executionTime;
 
+                                sumReliality += fittestPopulation.getReliability();
+                                sumCost += fittestPopulation.getCost();
+                                sumWeight += fittestPopulation.getWeight();
+
 
                             } else {
 //                                log("\nNot found any feasible solution :(");
@@ -83,10 +97,33 @@ public class Main {
 
                         log(String.format("%.2f %.2f %.2f",Config.alpha, Config.beta, Config.gamma));
                         log(Utility.printReport(resultIndividual, resultGeneration, resultTime) );
+
+
+                        strAlpha += String.format("%.2f,", Config.alpha);
+                        strBeta  += String.format("%.2f,", Config.beta);
+                        strGamma += String.format("%.2f,", Config.gamma);
+                        strReliability += String.format("%.6f,", sumReliality/n);
+                        strCost += String.format("%.2f,", sumCost/n);
+                        strWeight += String.format("%.2f,", sumWeight/n);
+
                     }
                 }
             }
         }
+
+        strAlpha = String.format("alpha = [%s]",strAlpha).replace(",]","];");
+        strBeta = String.format("beta = [%s]",strBeta).replace(",]","];");
+        strGamma = String.format("gamma = [%s]",strGamma).replace(",]","];");
+        strReliability = String.format("reliability = [%s]",strReliability).replace(",]","];");
+        strCost = String.format("cost = [%s]",strCost).replace(",]","];");
+        strWeight = String.format("weight = [%s]",strWeight).replace(",]","];");
+
+        log(strAlpha);
+        log(strBeta);
+        log(strGamma);
+        log(strReliability);
+        log(strCost);
+        log(strWeight);
 
 
     }
