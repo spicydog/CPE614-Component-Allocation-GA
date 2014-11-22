68ac0c73628c0e28605356b4ce8af81e1780d27e
Spicydog Proxy <spicydog@gmail.com>
2014-11-22 16:53:08 +0700

    Add BruteForce and GA for reporting

---
+++ src/org/spicydog/BruteForce.java
@@ -0,0 +1,41 @@
+package org.spicydog;
+
+import com.sun.xml.internal.ws.util.StringUtils;
+
+import static org.spicydog.Utility.log;
+
+public class BruteForce {
+
+    public static void main(String[] args) {
+
+
+        long startTime = System.nanoTime();
+
+        double maxFitness = Double.MIN_VALUE;
+        String bestGene = "";
+        for (int i = 0; i < Math.pow(2,Config.geneLength); i++) {
+            String genes = Integer.toBinaryString(i);
+            String zeros = new String(new char[Config.geneLength-genes.length()]).replace("\0", "0");
+            genes = String.format("%s%s",zeros,genes);
+
+            Individual individual = new Individual(Utility.convertStringToBoolean(genes));
+            if(individual.getFitness()>maxFitness) {
+                maxFitness = individual.getFitness();
+                bestGene = genes;
+                log(genes + " " + maxFitness);
+            }
+        }
+        long endTime = System.nanoTime();
+
+        Individual individual = new Individual(Utility.convertStringToBoolean(bestGene));
+        log("DONE!");
+        double executionTime = (double)(endTime-startTime)/1e9;
+        log(String.format("Execution Time: %.4f",executionTime));
+        log(String.format("fitness: %.6f",individual.getFitness()));
+        log(String.format("reliability: %.6f",individual.getReliability()));
+        log(String.format("cost: %.0f",individual.getCost()));
+        log(String.format("weight: %.0f",individual.getWeight()));
+        log(bestGene);
+
+    }
+}
---
+++ src/org/spicydog/GeneticAlgorithm.java
@@ -0,0 +1,87 @@
+package org.spicydog;
+
+import static org.spicydog.Utility.log;
+
+public class GeneticAlgorithm {
+
+
+    public static void main(String[] args) {
+
+            int n = Config.nRun;
+            int[] resultGeneration = new int[n];
+            double[] resultTime = new double[n];
+            Individual[] resultIndividual = new Individual[n];
+
+            double sumReliality = 0;
+            double sumCost = 0;
+            double sumWeight = 0;
+
+
+            for (int iRun = 0; iRun < n; iRun++) {
+
+                //log("\nRun: " + iRun+1);
+
+                double fitness = Double.MIN_VALUE;
+                double lastFitness = fitness;
+                boolean isSolutionFound = false;
+                int solutionFoundAtGeneration = -1;
+
+                long startTime = System.nanoTime();
+                long endTime = startTime;
+
+                // Create an initial population
+                Population myPopulation = new Population(Config.populationSize, true);
+
+                int generationCount = 0;
+                for (int iGeneration = 0; iGeneration < Config.maxGeneration; iGeneration++) {
+                    generationCount++;
+                    fitness = myPopulation.getFittest().getFitness();
+                    if(fitness>lastFitness) {
+                        endTime = System.nanoTime();
+                        isSolutionFound = true;
+                        solutionFoundAtGeneration = iGeneration;
+                        lastFitness = fitness;
+                        log("New solution: " + generationCount + "\t\tFittest: " + String.format("%.6f",fitness) + " *");
+                    }
+                    if(iGeneration%1000==0) {
+                        log("Generation: " + generationCount + "\t\tFittest: " + String.format("%.6f",fitness));
+                    }
+                    myPopulation = Algorithm.evolvePopulation(myPopulation);
+                }
+
+                if(isSolutionFound) {
+                    double executionTime = (double)(endTime-startTime)/1e9 ;
+                    Individual fittestPopulation = myPopulation.getFittest();
+                    log("\nSolution found :)");
+                    log("At generation: " + solutionFoundAtGeneration);
+                    log("Fitness: " + fittestPopulation.getFitness());
+                    log("Reliability: " + fittestPopulation.getReliability());
+                    log("Cost: " + fittestPopulation.getCost());
+                    log("Weight: " + fittestPopulation.getWeight());
+                    log("Execution Time: " + executionTime);
+                    log("Genes:");
+                    log(Utility.printSystem(fittestPopulation.toBooleans()));
+
+                    resultIndividual[iRun] = fittestPopulation;
+                    resultGeneration[iRun] = solutionFoundAtGeneration;
+                    resultTime[iRun] = executionTime;
+
+                    sumReliality += fittestPopulation.getReliability();
+                    sumCost += fittestPopulation.getCost();
+                    sumWeight += fittestPopulation.getWeight();
+
+
+                } else {
+                    log("\nNot found any feasible solution :(");
+                    log("Let's try again!");
+                    iRun--;
+
+                }
+            }
+
+            log(String.format("%.2f %.2f %.2f",Config.alpha, Config.beta, Config.gamma));
+            log(Utility.printReport(resultIndividual, resultGeneration, resultTime) );
+
+
+    }
+}
